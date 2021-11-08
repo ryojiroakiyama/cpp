f167a94a1b379848514bbfece3712c048b293191
rakiyama <ryojiro25@gmail.com>
2021-11-08 11:22:26 +0900

    done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			size_t j = 0;
+			while (argv[i][j])
+			{
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+				j++;
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
