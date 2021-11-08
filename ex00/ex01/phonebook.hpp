160178bb7248eec6634d520cbc2dbfdc7a2a2a1d
rakiyama <ryojiro25@gmail.com>
2021-11-09 00:18:34 +0900

    done base

--- ex00/ex01/phonebook.hpp
+++ ex00/ex01/phonebook.hpp
@@ -2,22 +2,26 @@
 #define PHONEBOOK_HPP
 #include "contact.hpp"
 
-#define FNAME "first name"
-#define LNAME "last name"
-#define NNAME "nick name"
-#define PNUM "phone number"
-#define DSECRET "darkest secret"
+#define FNAME static_cast<std::string>("first name")
+#define LNAME static_cast<std::string>("last name")
+#define NNAME static_cast<std::string>("nick name")
+#define PNUM static_cast<std::string>("phone number")
+#define DSECRET static_cast<std::string>("darkest secret")
+#define COLON static_cast<std::string>(" : ")
 
 class PhoneBook
 {
 	private:
-		Contact contacts[8];
-		int		head_idx;
-		int		last_idx;
-		int		size;
+		Contact 	contacts[8];
+		int			head_idx;
+		int			last_idx;
+		int			size;
+		std::string	get_input(std::string item, int width);
+		void		display_tenw(std::string content, std::string separate);
+		void		display_oneline(int order);
+		void		display_detail(int order);
 	public:
 		PhoneBook();
-		std::string	get_input(std::string item);
 		void		add();
 		void		search();
 		void		reception();
