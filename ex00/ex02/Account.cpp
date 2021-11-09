4a153da46c1bfa54633784392a122d664a83569f
rakiyama <ryojiro25@gmail.com>
2021-11-09 17:27:41 +0900

    done ex02

---
+++ ex00/ex02/Account.cpp
@@ -0,0 +1,134 @@
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+: _accountIndex(_nbAccounts++), _amount(initial_deposit),
+  _nbDeposits(0),	_nbWithdrawals(0)
+{
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";"
+				<< "created"
+				<< std::endl;
+	_totalAmount += _amount;
+}
+
+Account::~Account()
+{
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex << ";"
+				<< "amount:" << checkAmount() << ";"
+				<< "closed"
+				<< std::endl;
+}
+
+void	Account::_displayTimestamp()
+{
+	time_t		current = time(0);
+	const tm*	local = localtime(&current);
+	std::cout	<< "["
+				<< local->tm_year + 1900
+				<< std::setw(2) << std::setfill('0')
+				<< local->tm_mon + 1
+				<< std::setw(2) << std::setfill('0')
+				<< local->tm_mday
+				<< "_"
+				<< std::setw(2) << std::setfill('0')
+				<< local->tm_hour
+				<< std::setw(2) << std::setfill('0')
+				<< local->tm_min
+				<< std::setw(2) << std::setfill('0')
+				<< local->tm_sec
+				<< "] ";
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout	<< "accounts:" << getNbAccounts() << ";"
+				<< "total:" << getTotalAmount() << ";"
+				<< "deposits:" << getNbDeposits() << ";"
+				<< "withdrawals:" << getNbWithdrawals()
+				<< std::endl;
+}
+
+void	Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";"
+				<< "deposits:" << _nbDeposits << ";"
+				<< "withdrawals:" << _nbWithdrawals
+				<< std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout	<< "index:" << _accountIndex << ";"
+				<< "p_amount:" << _amount << ";"
+				<< "deposit:" << deposit << ";";
+	_amount += deposit;
+	_totalAmount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	std::cout	<< "amount:" << _amount << ";"
+				<< "nb_deposits:" << _nbDeposits
+				<< std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	_displayTimestamp();
+		std::cout	<< "index:" << _accountIndex << ";"
+					<< "p_amount:" << _amount << ";"
+					<< "withdrawal:";
+	if (_amount < withdrawal)
+	{
+		std::cout	<< "refused" << std::endl;
+		return false;
+	}
+	else
+	{
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		std::cout	<< withdrawal << ";"
+					<< "amount:" << _amount << ";"
+					<< "nb_withdrawals:" << _nbWithdrawals
+					<< std::endl;
+		return true;
+	}
+}
+
+int	Account::checkAmount() const
+{
+	return _amount;
+}
+
+int	Account::getNbAccounts()
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount()
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits()
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals()
+{
+	return _totalNbWithdrawals;
+}
---
+++ ex00/ex02/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+BOLD="\033[1m"
+UNDERLINE="\033[4m"
+BOLD_UNDERLINE="\033[1;4m"
+RESET="\033[0m"
+
+./bank > test.log
+printf "${GREEN}without close${RESET}\n"
+diff -y <(cat 19920104_091532.log | cut -d " " -f 2 | grep -v "close" | sort) <(cat test.log | cut -d " " -f 2 | grep -v "close" | sort)
+printf "${GREEN}about close${RESET}\n"
+diff -y <(cat 19920104_091532.log | cut -d " " -f 2 | grep "close" | sort) <(cat test.log | cut -d " " -f 2 | grep "close" | sort)
