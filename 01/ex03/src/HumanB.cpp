063af45ce28e065c6f0feeeb951476b1195fbfbd
rakiyama <ryojiro25@gmail.com>
2021-11-10 15:42:48 +0900

    done ex02

--- 01/ex03/src/HumanB.cpp
+++ 01/ex03/src/HumanB.cpp
@@ -7,12 +7,15 @@ HumanB::HumanB(std::string name) : _name(name), _weapon(0)
 
 void	HumanB::attack()
 {
-	std::cout	<< CYAN
-				<< _name
-				<< " attacks with his "
-				<< _weapon->getType()
-				<< RESET
-				<< std::endl;
+	std::cout		<< CYAN
+					<< _name
+					<< " attacks with his ";
+	if (_weapon)
+		std::cout	<< _weapon->getType();
+	else
+		std::cout	<< "fist";
+	std::cout		<< RESET
+					<< std::endl;
 }
 
 void	HumanB::setWeapon(Weapon& weapon)
