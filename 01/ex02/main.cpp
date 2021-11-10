6bf1bc702d369c9f08840248f02ff20531058fc0
rakiyama <ryojiro25@gmail.com>
2021-11-10 12:51:15 +0900

    done simple ex02

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include "escape_sequence.hpp"
+
+int main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout	<< "address" << std::endl
+				<< MAGENTA
+				<< std::setw(10) << "str:" << &str << std::endl
+				<< std::setw(10) << "stringPTR:" << stringPTR << std::endl
+				<< std::setw(10) << "stringREF:" << &stringREF << std::endl
+				<< RESET
+				<< "string" << std::endl
+				<< CYAN
+				<< std::setw(10) << "stringPTR:" << *stringPTR << std::endl
+				<< std::setw(10) << "stringREF:" << stringREF << std::endl
+				<< RESET;
+}
