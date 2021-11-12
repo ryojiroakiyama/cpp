f2024e2ad1ec6ff692f82639c5592878fdb2295c
rakiyama <ryojiro25@gmail.com>
2021-11-12 18:56:24 +0900

    done simple c02/ex02

--- 02/ex02/src/Fixed.cpp
+++ 02/ex02/src/Fixed.cpp
@@ -4,34 +4,64 @@
 
 const int	Fixed::_FixBit = 8;
 
+// constructor, destructor
 Fixed::Fixed() : _InternalValue(0) {}
 Fixed::Fixed( const int value ) { _InternalValue = value << _FixBit; }
 Fixed::Fixed( const float value ) { _InternalValue = roundf(value * std::pow(2, 8)); }
 Fixed::~Fixed() {}
 Fixed::Fixed( const Fixed& original ) { *this = original; }
 
+// basic method
 int		Fixed::getRawBits( void ) const { return _InternalValue; }
 void	Fixed::setRawBits( int const raw ) { _InternalValue = raw; }
 float	Fixed::toFloat( void ) const { return static_cast<float>(_InternalValue) / (1 << _FixBit); }
 int		Fixed::toInt( void ) const { return _InternalValue >> _FixBit; }
 
-Fixed&	Fixed::operator=( const Fixed& right )
+// overload operator
+Fixed&	Fixed::operator= ( const Fixed& right ) { _InternalValue = right.getRawBits(); return *this; }
+bool	Fixed::operator> ( const Fixed& right ) const { return _InternalValue > right.getRawBits(); }
+bool	Fixed::operator< ( const Fixed& right ) const { return _InternalValue < right.getRawBits(); }
+bool	Fixed::operator>=( const Fixed& right ) const { return _InternalValue >= right.getRawBits(); }
+bool	Fixed::operator<=( const Fixed& right ) const { return _InternalValue <= right.getRawBits(); }
+bool	Fixed::operator==( const Fixed& right ) const { return _InternalValue == right.getRawBits(); }
+bool	Fixed::operator!=( const Fixed& right ) const { return _InternalValue != right.getRawBits(); }
+Fixed	Fixed::operator+ ( const Fixed& right ) const { return Fixed(_InternalValue + right.getRawBits()); }
+Fixed	Fixed::operator- ( const Fixed& right ) const { return Fixed(_InternalValue - right.getRawBits()); }
+Fixed	Fixed::operator* ( const Fixed& right ) const { return Fixed(_InternalValue * right.getRawBits()); }
+Fixed	Fixed::operator/ ( const Fixed& right ) const { return Fixed(_InternalValue / right.getRawBits()); }
+Fixed&	Fixed::operator++() { _InternalValue++; return *this; }
+Fixed	Fixed::operator++(int) { Fixed original = *this; ++(*this); return original; }
+
+// min, max
+Fixed&			Fixed::min( Fixed& a, Fixed& b )
 {
-	_InternalValue = right._InternalValue;
-	return *this;
+	if (a > b)
+		return a;
+	return a;
 }
 
-bool	Fixed::operator>( const Fixed& right ) { return _InternalValue > right.getRawBits(); }
-bool	Fixed::operator<( const Fixed& right ) { return _InternalValue < right.getRawBits(); }
-bool	Fixed::operator>=( const Fixed& right ) { return _InternalValue >= right.getRawBits(); }
-bool	Fixed::operator<=( const Fixed& right ) { return _InternalValue <= right.getRawBits(); }
-bool	Fixed::operator==( const Fixed& right ) { return _InternalValue == right.getRawBits(); }
-bool	Fixed::operator!=( const Fixed& right ) { return _InternalValue != right.getRawBits(); }
-Fixed	Fixed::operator+( const Fixed& right ) { return Fixed(_InternalValue + right.getRawBits()); }
-Fixed	Fixed::operator-( const Fixed& right ) { return Fixed(_InternalValue - right.getRawBits()); }
-Fixed	Fixed::operator*( const Fixed& right ) { return Fixed(_InternalValue * right.getRawBits()); }
-Fixed	Fixed::operator/( const Fixed& right ) { return Fixed(_InternalValue / right.getRawBits()); }
+const Fixed&	Fixed::min( const Fixed& a, const Fixed& b )
+{
+	if (a > b)
+		return b;
+	return a;
+}
 
+Fixed&			Fixed::max( Fixed& a, Fixed& b )
+{
+	if (a < b)
+		return b;
+	return a;
+}
+
+const Fixed& 	Fixed::max( const Fixed& a, const Fixed& b )
+{
+	if (a < b)
+		return b;
+	return a;
+}
+
+// external function
 std::ostream&	operator<<( std::ostream& os, const Fixed& right )
 {
 	os << right.toFloat();
--- 02/ex02/src/main.cpp
+++ 02/ex02/src/main.cpp
@@ -3,17 +3,14 @@
 
 int main( void )
 {
-	Fixed	a(42.42f);
-	Fixed	b(222);
-	std::cout << (a > b) << std::endl;
-	//Fixed a;
-	//Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	//std::cout << a << std::endl;
-	//std::cout << ++a << std::endl;
-	//std::cout << a << std::endl;
-	//std::cout << a++ << std::endl;
-	//std::cout << a << std::endl;
-	//std::cout << b << std::endl;
-	//std::cout << Fixed::max( a, b ) << std::endl;
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
 }
