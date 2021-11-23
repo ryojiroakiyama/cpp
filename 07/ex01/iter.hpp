839fb9fe5bcaf252c183b28c9a17e5cb202958b7
rakiyama <ryojiro25@gmail.com>
2021-11-23 23:36:31 +0900

    done ex01

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -3,17 +3,24 @@
 
 #include <iostream>
 
-template <typename T>
-void	iter(const T array[], const size_t length, void (*function)(T))
+template <typename T, typename U>
+void	iter(T array[], const size_t length, U (*function)(T&))
 {
 	for (size_t idx = 0; idx < length; idx++)
 		function(array[idx]);
 }
 
 template <typename T>
-void	put_element(T element)
+void		put_element(T element)
 {
 	std::cout << element << std::endl;
+}
+
+template <typename T>
+std::string	twice(T& element)
+{
+	element *= 2;
+	return "twiceeeeee";
 }
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -5,10 +5,15 @@ int main( void )
 {
 	const int length = 5;
 	int	array[length] = {0, 1, 2, 3, 4};
-	iter(array, length, put_element);
+	iter<int, void>(array, length, put_element);
+	std::cout << "^^^^^^^^^before twice" << std::endl;
+	iter<int, std::string>(array, length, twice);
+	iter<int, void>(array, length, put_element);
+	std::cout << "^^^^^^^^^after twice" << std::endl;
 
-	iter("hello", 5, put_element);
+	iter<const char, void>("hello", 5, put_element);
+	std::cout << "^^^^^^^^^" << std::endl;
 
 	std::string str[length] = {"abc", "def", "ghi", "jkl", "mno"};
-	iter(str, length, put_element);
+	iter<std::string, void>(str, length, put_element);
 }
