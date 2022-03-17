261b56804848af6e4dccbc382d28ff8232d0ca9c
rakiyama <ryojiro25@gmail.com>
2022-03-17 20:21:31 +0900

    add: base

--- 08/ex02/mutantstack.hpp
+++ 08/ex02/mutantstack.hpp
@@ -1,19 +1,38 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <list>
+#include <stack>
 
 template <typename T>
-class mutantstack
+class mutantstack : public std::stack<T>
 {
 	public:
 		mutantstack();
 		~mutantstack();
 		mutantstack(mutantstack const &other);
 		mutantstack &operator=(mutantstack const &other);
-
-	private:
-		std::list<T> container;
 };
+
+template <typename T>
+mutantstack<T>::mutantstack() {}
+
+template <typename T>
+mutantstack<T>::~mutantstack() {}
+
+template <typename T>
+mutantstack<T>::mutantstack(mutantstack<T> const &other)
+{
+	*this = other;
+}
+
+template <typename T>
+mutantstack<T> &mutantstack<T>::operator=(mutantstack<T> const &other)
+{
+	if (this != &other)
+	{
+		// mutant's specific copy treatment
+	}
+	return *this;
+}
 
 #endif /* MUTANTSTACK_HPP */
