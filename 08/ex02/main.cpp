9dd7cf4555252ad97cab650f374ca1dc0d1679dd
rakiyama <ryojiro25@gmail.com>
2022-03-18 16:38:04 +0900

    add: test

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -1,55 +1,57 @@
 #include "mutantstack.hpp"
+#include "escape_sequence.hpp"
 #include "test.hpp"
 #include <iostream>
 #include <stack>
 #include <list>
 
+static void ColorMessage(const std::string color, const std::string message)
+{
+	std::cout << color << message << RESET << std::endl;
+}
+
 int main()
 {
+	/* provided test */
+	//{
+	//	MutantStack<int> mstack;
+	//	mstack.push(5);
+	//	mstack.push(17);
+	//	std::cout << mstack.top() << std::endl;
+	//	mstack.pop();
+	//	std::cout << mstack.size() << std::endl;
+	//	mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
+	//	MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
+	//	++it;
+	//	--it;
+	//	while (it != ite) {
+	//		std::cout << *it << std::endl;
+	//	++it; }
+	//	std::stack<int> s(mstack);
+	//}
+
+	/* Additional Test */
+	{
+		/* Stack */
+		std::cout << std::endl;
+		ColorMessage(GREEN, "Stack");
+		std::stack<int> oristack;
+		COMMONTEST(oristack);
+		OutAll(oristack);
+
 		/* MutantStack */
-		std::cout << "MutantStack" << std::endl;
+		std::cout << std::endl;
+		ColorMessage(GREEN, "MutantStack");
 		MutantStack<int> mstack;
-		mstack.push(1);
-		mstack.push(2);
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
-		std::cout << mstack.size() << std::endl;
-		mstack.push(2);
-		mstack.push(3);
-		mstack.push(4); //[...] mstack.push(0);
-		std::cout << "-------" << std::endl;
-		MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
-		++it;
-		--it;
-		while (it != ite) {
-			std::cout << *it << std::endl;
-		++it; }
+		COMMONTEST(mstack);
+		IteratorTest(mstack);
+		ReverseIteratorTest(mstack);
+		OutAll(mstack);
 
 		/* copy stack */
 		std::cout << std::endl;
-		std::cout << "copy stack" << std::endl;
+		ColorMessage(GREEN, "CopyStack");
 		std::stack<int> copystack(mstack);
 		OutAll(copystack);
-
-		/* stack */
-		std::cout << std::endl;
-		std::cout << "stack" << std::endl;
-		std::stack<int> oristack;
-		oristack.push(1);
-		oristack.push(2);
-		std::cout << oristack.top() << std::endl;
-		oristack.pop();
-		std::cout << oristack.size() << std::endl;
-		oristack.push(2);
-		oristack.push(3);
-		oristack.push(4);
-		std::cout << "-------" << std::endl;
-		OutAll(oristack);
-		//std::stack<int>::iterator it = oristack.begin(); std::stack<int>::iterator ite = oristack.end();
-		//++it;
-		//--it;
-		//while (it != ite) {
-		//	std::cout << *it << std::endl;
-		//++it; }
-		//std::stack<int> s(oristack); return 0;
+	}
 }
--- 08/ex02/test.hpp
+++ 08/ex02/test.hpp
@@ -1,17 +1,95 @@
 #include "mutantstack.hpp"
+#include <iostream>
+#include <iomanip>
+
+static void Title(std::string message)
+{
+	std::cout << "----" << message << "----" << std::endl;
+}
 
 template <typename T>
 void OutAll(std::stack<T> s)
 {
+	Title("OutAll");
 	while (!s.empty())
 	{
 		std::cout << s.top() << std::endl;
 		s.pop();
 	}
+	Title("OutAll");
+}
+
+const int w = 6;
+
+template <typename T, typename U>
+void Push(T& s, U content)
+{
+	std::cout << std::setw(w) << "push: " << content << std::endl;
+	s.push(content);
 }
 
 template <typename T>
-void Test()
+void Pop(T& s)
 {
-	/* template test */
+	std::cout << std::setw(w) << "pop  " << std::endl;
+	s.pop();
+}
+
+template <typename T>
+void Top(T& s)
+{
+	std::cout << std::setw(w) << "top: " << s.top() << std::endl;
+}
+
+template <typename T>
+void Size(T& s)
+{
+	std::cout << std::setw(w) << "size: " << s.size() << std::endl;
+}
+
+template <typename T>
+void COMMONTEST(T& s)
+{
+	Title("COMMONTEST");
+	Push(s, 1);
+	Push(s, 2);
+	Top(s);
+	Pop(s);
+	Size(s);
+	Push(s, 2);
+	Push(s, 3);
+	Push(s, 4);
+	Title("COMMONTEST");
+}
+
+template <typename T>
+void IteratorTest(T& s)
+{
+	Title("IteratorTest");
+	typename T::iterator it = s.begin();
+	typename T::iterator ite = s.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	Title("IteratorTest");
+}
+
+template <typename T>
+void ReverseIteratorTest(T& s)
+{
+	Title("ReverseIteratorTest");
+	typename T::reverse_iterator it = s.rbegin();
+	typename T::reverse_iterator ite = s.rend();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	Title("ReverseIteratorTest");
 }
